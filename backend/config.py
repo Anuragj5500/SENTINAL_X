cf342108c4ef333e49801c17940ed2c71b03d5a8
Anuragj5500 <anurag.jaiswal5500@gmail.com>
2026-07-17 23:06:30 +0530

    Add new CORS origin for production frontend

--- backend/config.py
+++ backend/config.py
@@ -61,7 +61,8 @@ class Settings(BaseSettings):
     SHODAN_API_KEY: Optional[str] = None
     
     # CORS
-    CORS_ORIGINS: list = ["http://localhost:5173", "http://localhost:3000", "http://localhost:8080"]
+        CORS_ORIGINS: list = ["http://localhost:5173", "http://localhost:3000", "http://localhost:8080","https://sentinelx-frontend-yx15.onrender.com", ]
+
     
     # Pagination
     DEFAULT_PAGE_SIZE: int = 50
